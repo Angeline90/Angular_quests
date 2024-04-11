81ad537cbee5b796e010759d02721af78508d6ac
Angeline90 <angeline.gauron@gmail.com>
2024-04-11 16:53:15 +0200

    quest completion

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
+  newUser: User = {
+    firstName: 'Votre prénom',
+    lastName: 'Votre nom',
+    email: 'exemple@form.fr',
+    password: '',
+  };
 
+  onSubmit(): void {
+    console.log(this.newUser);
+  }
 }
